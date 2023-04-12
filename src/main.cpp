ee94266d39b7b38f40bc65430e597fbdf3a24986
panhongtao <59553164+panhongtao07@users.noreply.github.com>
2023-04-12 19:52:44 +0800

    Update main.cpp
    使代码更易于理解

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,9 @@
-#include <memory>
 #include "./ai.h"
 #include "./parser.h"
 
 int main(int argc, char** argv) {
-    std::unique_ptr<AI> ai(Parser::parse(argc, argv));
+    AI* ai(Parser::parse(argc, argv));
     ai->sendRequest();
     ai->showResponse();
+    delete ai;
 }
